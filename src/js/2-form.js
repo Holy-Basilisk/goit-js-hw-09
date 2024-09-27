2d19cc7510ab4bb805f4560ce5b8840ba270399b
Paul <mailpiligrim@gmail.com>
2024-09-27 12:40:22 +0300

    HW9 done

---
+++ src/js/2-form.js
@@ -0,0 +1,34 @@
+const formData = {
+    email: "",
+    message: ""
+}
+
+const form = document.querySelector('.feedback-form');
+
+formData.email = JSON.parse(localStorage.getItem("formData")).email;
+form.email.value = formData.email;
+formData.message = JSON.parse(localStorage.getItem("formData")).message;
+form.message.value = formData.message;
+
+form.addEventListener('input', saveText);
+form.addEventListener("submit", event => {    
+    const email = event.target.elements.email.value;
+    const message = event.target.elements.message.value;
+
+    event.preventDefault();
+    if (email === '' || message === '') {
+        alert('Fill please all fields');
+    } else {
+        console.log(formData);
+        formData.email = '';
+        formData.message = '';
+        localStorage.setItem("formData", JSON.stringify(formData));
+        form.reset();
+    }
+})
+
+function saveText (event) {
+    formData[event.target.name] = event.target.value;
+    localStorage.setItem("formData", JSON.stringify(formData));
+}
+
